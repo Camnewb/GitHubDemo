13508f6f734c3671f40a47fde8b248c094b0deb8
Camnewb <camnewborn@gmail.com>
2018-10-10 15:21:49 -0400

    Added comments
    Added comments for modula clarification

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,8 +3,8 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             String str = "";
-            if (i % 3 == 0) str += "Fizz";
-            if (i % 5 == 0) str += "Buzz";
+            if (i % 3 == 0) str += "Fizz";//If i is divisible by 3
+            if (i % 5 == 0) str += "Buzz";//If i is divisible by 5
             if (!str.equals("")) System.out.println(str);
             else System.out.println(i);
         }
